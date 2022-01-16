7700ab3d10e4a52aec02ad4fc71eff2ff50a6e94
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-01-16 12:21:57 -0500

    commit tree 07c06b732fa8c06a8758632e8efd51bf1f8d3faa

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a){
+  let result;
+  if ( a <= 400 ) 
+    result = 'This one is on me!';
+  else if ( a > 400 && a < 2500)
+    result = 'I will gladly take your thirty bucks.';
+  else if ( a >= 2501)
+    result = 'No can do.';
+    return result;
+
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(a){
+  console.log(a)
+  return ( a === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(a){
+switch (a) {
+  case 'generous':
+    return 'Thank you so much.'
+  break;
+  case 'not as generous':
+    return 'Thank you.'
+  break;
+  default: 
+  return 'Bye.'
+}
+    
+
 }
